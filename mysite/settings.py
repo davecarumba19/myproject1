e19d838c1b29d8f377d71e30820cb59a20988882
Errol Dave Carumba <erroldave19@gmail.com>
2021-11-11 07:47:29 +0800

    second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9)!p4)u(zh=7((d0sr4+v528a)*usa0hn3*!lci4egg22()&by'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
